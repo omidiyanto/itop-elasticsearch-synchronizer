0745fbc9835382cdb221276f8a4daea8447a3c40
O Midiyanto <o.midiyanto@satnusa.com>
2025-08-01 15:38:14 +0700

    fix: timezon to UTC+0

--- main.go
+++ main.go
@@ -269,7 +269,7 @@ func mapTicketToES(t itop.Ticket, holidays map[string]struct{}, debug bool) ESTi
 
 	tz := os.Getenv("TIMEZONE")
 	if tz == "" {
-		tz = "Asia/Jakarta"
+		tz = "UTC"
 	}
 	loc, err := time.LoadLocation(tz)
 	if err != nil {
